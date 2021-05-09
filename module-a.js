7dab53b1d2e2494eb77e23bde529fd0b60358523
imalovelyghost <imalovelyghost13@gmail.com>
2021-05-09 17:50:56 +0200

    first-steps

---
+++ module-a.js
@@ -0,0 +1,23 @@
+//Arrow Functions
+//Arrow functions allows a short syntax for writing function expressions.
+//You don't need the function keyword, the return keyword, and the curly brackets.
+
+console.log("soc una pute")
+
+
+var x = 2;
+var y = 3;
+// ES5
+var x = function m(x, y) {
+    return x * y;
+
+}
+console.log(x)
+
+function m()
+
+var xx = 2;
+var yy = 3;
+// ES6
+const xxx = (xx, yy) => xx * yy;
+console.log(xxx)
